946ed36c8d7312d183e164e2c65863c484c5c451
Joshua Smart <josh@thesmarts.co.uk>
2024-08-30 14:12:08 +0100

    Configured adguard.home dns rewrite

--- modules/services/adguardhome.nix
+++ modules/services/adguardhome.nix
@@ -1,9 +1,10 @@
 { config, lib, ... }:
 let
   inherit (lib) mkIf;
+  cfg = config.services.adguardhome;
 in
 {
-  config = mkIf config.services.adguardhome.enable {
+  config = mkIf cfg.enable {
     # Open ports for DNS server
     networking.firewall = {
       allowedTCPPorts = [ 53 ];
@@ -12,8 +13,14 @@ in
 
     services.adguardhome = {
       openFirewall = true;
-      port = 80;
+      port = 3000;
+      host = "0.0.0.0";
       allowDHCP = true;
+    };
+
+    services.nginx.proxyHosts."adguard.home" = {
+      port = cfg.port;
+      ssl = false;
     };
   };
 }
